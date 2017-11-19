670ab06e8f992de32d877664d3324a692f68ec7f
Ginungagap <ilya.dog84@gmail.com>
2017-11-19 18:10:30 +0200

    correction

--- engine.js
+++ engine.js
@@ -38,20 +38,19 @@ function CheckVictory() {
 function CompTurn() {
   
   for (i=0; i<9; i++) if (cell[i] == 0) PutHere = i;
-  if (cell[4] == 0) PutHere = 4;
-  if (cell[0] == cell[4] && cell[8] == 'player' && cell[0] == 0) PutHere = 0;
+  
   if (cell[1] == cell[3] && cell[3] == 'player') PutHere = 0;
   if (cell[7] == cell[3] && cell[3] == 'player') PutHere = 6;
   if (cell[4] == cell[0] && cell[4] == 'player') PutHere = 6;
   if (cell[2] == cell[6] && cell[2] == 'player') PutHere = 7;
   if (cell[1] == cell[3] && cell[1] == cell[8] && cell[1] == 'player') PutHere = 7;
   if (cell[7] == cell[3] && cell[7] == cell[2] && cell[7] == 'player') PutHere = 1;
+  if (cell[4] == 0) PutHere = 4;
   
   if (cell[0] == cell[1] && cell[2] == 0 && cell[0] !== 0) PutHere = 2;
   if (cell[0] == cell[2] && cell[1] == 0 && cell[0] !== 0) PutHere = 1;
   if (cell[1] == cell[2] && cell[0] == 0 && cell[2] !== 0) PutHere = 0;
   if (cell[3] == cell[4] && cell[5] == 0 && cell[3] !== 0) PutHere = 5;
-  if (cell[3] == cell[5] && cell[4] == 0 && cell[3] !== 0) PutHere = 4;
   if (cell[4] == cell[5] && cell[3] == 0 && cell[5] !== 0) PutHere = 3;
   if (cell[6] == cell[7] && cell[8] == 0 && cell[6] !== 0) PutHere = 8;
   if (cell[6] == cell[8] && cell[7] == 0 && cell[6] !== 0) PutHere = 7;
@@ -61,17 +60,14 @@ function CompTurn() {
   if (cell[6] == cell[0] && cell[3] == 0 && cell[6] !== 0) PutHere = 3;
   if (cell[3] == cell[0] && cell[6] == 0 && cell[3] !== 0) PutHere = 6;
   if (cell[7] == cell[4] && cell[1] == 0 && cell[7] !== 0) PutHere = 1;
-  if (cell[7] == cell[1] && cell[4] == 0 && cell[7] !== 0) PutHere = 4;
   if (cell[4] == cell[1] && cell[7] == 0 && cell[4] !== 0) PutHere = 7;
   if (cell[8] == cell[5] && cell[2] == 0 && cell[8] !== 0) PutHere = 2;
   if (cell[8] == cell[2] && cell[5] == 0 && cell[8] !== 0) PutHere = 5;
   if (cell[5] == cell[2] && cell[8] == 0 && cell[5] !== 0) PutHere = 8;
 
   if (cell[6] == cell[4] && cell[2] == 0 && cell[6] !== 0) PutHere = 2;
-  if (cell[6] == cell[2] && cell[4] == 0 && cell[6] !== 0) PutHere = 4;
   if (cell[4] == cell[2] && cell[6] == 0 && cell[4] !== 0) PutHere = 6;
   if (cell[0] == cell[4] && cell[8] == 0 && cell[0] !== 0) PutHere = 8;
-  if (cell[0] == cell[8] && cell[4] == 0 && cell[0] !== 0) PutHere = 4;
   if (cell[4] == cell[8] && cell[0] == 0 && cell[4] !== 0) PutHere = 0;
   
   ai(PutHere, 'ai');
